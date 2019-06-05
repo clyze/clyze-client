53b5c86b1d45c02e07fcf01db761fcf7083de8fa
George Fourtounis <gfour@di.uoa.gr>
2019-06-05 19:31:35 +0300

    Move method to clue-common.

--- src/main/groovy/org/clyze/client/SourceProcessor.groovy
+++ src/main/groovy/org/clyze/client/SourceProcessor.groovy
@@ -1,14 +1,6 @@
 package org.clyze.client
 
 import static groovy.io.FileType.FILES
-import java.io.FileInputStream
-import java.nio.charset.Charset
-import java.nio.ByteBuffer
-import java.nio.CharBuffer
-import java.nio.file.Path
-import java.nio.file.Files
-import org.apache.commons.io.IOUtils
-import org.mozilla.universalchardet.UniversalDetector
 
 /**
  * Analyzes the sources of a project and optionally converts them to
@@ -18,9 +10,6 @@ import org.mozilla.universalchardet.UniversalDetector
  * encodings to the jcPlugin.
  */
 class SourceProcessor {
-
-    // Encoding detector.
-    UniversalDetector detector = new UniversalDetector(null);
 
     /**
      * Arguments:
@@ -86,47 +75,6 @@ class SourceProcessor {
                  lineCount          : lineCount,
                  foundAndroidSource : foundAndroidSource
                ]
-    }
-
-    // If the given file is not encoded as UTF-8, its encoding is
-    // detected and its contents are converted to UTF-8.
-    private void ensureUTF8(String filename) {
-        FileInputStream fis = new FileInputStream(filename)
-
-        byte[] buf = new byte[4096];
-        int nread;
-        while ((nread = fis.read(buf)) > 0 && !detector.isDone()) {
-            detector.handleData(buf, 0, nread);
-        }
-        detector.dataEnd()
-
-        String encoding = detector.getDetectedCharset()
-        detector.reset()
-        fis.close()
-
-        if ((encoding == null) || (encoding.equals("UTF-8"))) {
-            return
-        }
-
-        // Try to convert source file to UTF-8.
-        try {
-            Charset sourceEncoding = Charset.forName(encoding)
-            Charset targetEncoding = Charset.forName("UTF-8")
-            byte[] buf2 = IOUtils.toByteArray(new FileInputStream(filename))
-            CharBuffer data = sourceEncoding.decode(ByteBuffer.wrap(buf2))
-            ByteBuffer outBuf = targetEncoding.encode(data)
-            def bufWriter = new BufferedWriter(new FileWriter(filename))
-            int outDataLength = 0
-            while (outBuf.remaining() > 0) {
-                bufWriter.write(outBuf.get())
-                outDataLength++
-                    }
-            bufWriter.close()
-            println "Converted ${encoding} to UTF-8: ${filename}, ${buf2.length} vs ${outDataLength} bytes"
-        } catch (Exception ex) {
-            ex.printStackTrace()
-            throw new RuntimeException("Cannot convert encoding " + encoding + " to UTF-8")
-        }
     }
 
     private static boolean isAndroidSource(String path) {
